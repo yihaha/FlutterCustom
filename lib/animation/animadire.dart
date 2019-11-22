4c5871f9b2f377b066da7fa6c71a0cd2cebb0fad
benhaoyi <benhaoyi@aijiatui.com>
2019-11-22 14:40:43 +0800

    视频tab红点完成

---
+++ lib/animation/animadire.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class AnimationDirectory extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => AnimaDirState();
+}
+
+class AnimaDirState extends State<AnimationDirectory> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
--- lib/animation/tween1.dart
+++ lib/animation/tween1.dart
@@ -16,18 +16,20 @@ class _TweenAni1 extends State<TweenAni1> with SingleTickerProviderStateMixin {
     super.initState();
     _controller = AnimationController(
         duration: Duration(milliseconds: 2000), vsync: this);
-    _animation = Tween(begin: 10.0, end: 200.0).animate(_controller)
-      ..addListener(() {
-        setState(() {});
-      })
-      ..addStatusListener((status) {
-        print('$status');
-        if (status == AnimationStatus.completed) {
-          _controller.reverse();
-        } else if (status == AnimationStatus.dismissed) {
-          _controller.forward();
-        }
-      });
+//    _animation = Tween(begin: 10.0, end: 200.0).animate(_controller)
+//      ..addListener(() {
+//        setState(() {});
+//      })
+//      ..addStatusListener((status) {
+//        print('$status');
+//        if (status == AnimationStatus.completed) {
+//          _controller.reverse();
+//        } else if (status == AnimationStatus.dismissed) {
+//          _controller.forward();
+//        }
+//      });
+    ///并行动画
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
 
     _controller.forward(); //开始动画
   }
@@ -36,8 +38,19 @@ class _TweenAni1 extends State<TweenAni1> with SingleTickerProviderStateMixin {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: AnimatedRed(_animation),
+        ///并行动画
+        child: AnimatedRed2(_animation),
 
+        ///方式3
+//        child: YAnimaBuilder(
+//          child: FlutterLogo(),
+//          animation: _animation,
+//        ),
+
+        ///方式2
+//        child: AnimatedRed(_animation),
+
+        ///方式1
 //        Container(
 //          color: Colors.white,
 //          margin: EdgeInsets.symmetric(vertical: 10.0),
@@ -56,6 +69,7 @@ class _TweenAni1 extends State<TweenAni1> with SingleTickerProviderStateMixin {
   }
 }
 
+///AnimatedWidget方式
 class AnimatedRed extends AnimatedWidget {
   AnimatedRed(Animation<double> animation) : super(listenable: animation);
 
@@ -68,6 +82,55 @@ class AnimatedRed extends AnimatedWidget {
         height: _animation.value,
         width: _animation.value,
         child: FlutterLogo(),
+      ),
+    );
+  }
+}
+
+///AnimatedWidget方式 并行动画
+class AnimatedRed2 extends AnimatedWidget {
+  final _opacityTween = Tween<double>(begin: 0.1, end: 1.0);
+  final _sizeTween = Tween<double>(begin: 0.0, end: 300.0);
+
+  AnimatedRed2(Animation<double> animation) : super(listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    Animation<double> _animation = listenable;
+    return Center(
+      child: Opacity(
+        opacity: _opacityTween.evaluate(_animation),
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 10.0),
+          height: _sizeTween.evaluate(_animation),
+          width: _sizeTween.evaluate(_animation),
+          child: FlutterLogo(),
+        ),
+      ),
+    );
+  }
+}
+
+///AnimatedBuilder方式
+class YAnimaBuilder extends StatelessWidget {
+  final Widget child;
+  final Animation<double> animation;
+
+  YAnimaBuilder({this.child, this.animation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (BuildContext context, Widget child) {
+          return Container(
+            height: animation.value,
+            width: animation.value,
+            child: child,
+          );
+        },
+        child: child,
       ),
     );
   }
